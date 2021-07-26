0870f885c0c05c3aac99c18e1de1da96422c6495
Francesco Dente <dente.francesco1996@gmail.com>
2021-07-26 16:46:00 +0200

    Fix abstract role initialization

--- EasyDesk.CleanArchitecture.Domain/Roles/AbstractRole.cs
+++ EasyDesk.CleanArchitecture.Domain/Roles/AbstractRole.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
 using static EasyDesk.Tools.Collections.ImmutableCollections;
 
 namespace EasyDesk.CleanArchitecture.Domain.Roles
@@ -16,16 +17,17 @@ namespace EasyDesk.CleanArchitecture.Domain.Roles
         private static readonly IEqualityComparer<AbstractRole<T>> _equalityComparer =
             EqualityComparers.FromProperties<AbstractRole<T>>(x => x.Id);
 
-        private static readonly IImmutableDictionary<string, T> _rolesById;
+        private static readonly Lazy<IImmutableDictionary<string, T>> _rolesById;
 
         static AbstractRole()
         {
             var roleType = typeof(T);
-            _rolesById = Map(roleType
-                .GetProperties(BindingFlags.Static)
+            var properties = roleType
+                .GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly);
+            _rolesById = new(() => Map(properties
                 .Where(p => p.PropertyType.Equals(roleType))
                 .Select(p => p.GetValue(null) as T)
-                .Select(r => (r.Id.ToString(), r)));
+                .Select(r => (r.ToString(), r))), LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
         public AbstractRole(RoleId id)
@@ -36,10 +38,13 @@ namespace EasyDesk.CleanArchitecture.Domain.Roles
         public RoleId Id { get; }
 
         public static T From(string roleId) => _rolesById
+            .Value
             .GetOption(roleId)
             .OrElseThrow(() => new ArgumentException($"The given RoleId is not a valid {typeof(T).Name}", nameof(roleId)));
 
         public static T FromRoleId(RoleId roleId) => From(roleId.ToString());
+
+        public static IEnumerable<T> List() => _rolesById.Value.Values;
 
         public override bool Equals(object other) => _equalityComparer.Equals(this, other as T);
 
---
+++ EasyDesk.CleanArchitecture.UnitTests/Domain/Roles/AbstractRoleTests.cs
@@ -0,0 +1,62 @@
+﻿using EasyDesk.CleanArchitecture.Domain.Roles;
+using Shouldly;
+using System;
+using Xunit;
+using static EasyDesk.Tools.Collections.EnumerableUtils;
+
+namespace EasyDesk.CleanArchitecture.UnitTests.Domain.Roles
+{
+    public class AbstractRoleTests
+    {
+        private const string _primaryRole = "PrimaryRole";
+        private const string _secondaryRole = "SecondaryRole";
+
+        private class TestRole : AbstractRole<TestRole>
+        {
+            public TestRole(RoleId roleId) : base(roleId)
+            {
+
+            }
+
+            public static TestRole PrimaryRole { get; } = new(RoleId.From(_primaryRole));
+
+            public static TestRole SecondaryRole { get; } = new(RoleId.From(_secondaryRole));
+        }
+
+        [Fact]
+        public void ToString_ShouldReturnTheRoleIdAsAString()
+        {
+            TestRole.PrimaryRole.ToString().ShouldBe(_primaryRole);
+        }
+
+        [Fact]
+        public void List_ShouldReturnAllDeclaredRoles()
+        {
+            TestRole.List().ShouldBe(Items(TestRole.PrimaryRole, TestRole.SecondaryRole), ignoreOrder: true);
+        }
+
+        [Fact]
+        public void From_ShouldReturnTheCorrespondingRole_IfItExists()
+        {
+            TestRole.From(TestRole.PrimaryRole.ToString()).ShouldBe(TestRole.PrimaryRole);
+        }
+
+        [Fact]
+        public void From_ShouldFail_IfTheRoleIdDoesNotExist()
+        {
+            Should.Throw<ArgumentException>(() => TestRole.From("IncorrectRole"));
+        }
+
+        [Fact]
+        public void FromRoleId_ShouldReturnTheCorrespondingRole_IfItExists()
+        {
+            TestRole.From(TestRole.PrimaryRole.Id).ShouldBe(TestRole.PrimaryRole);
+        }
+
+        [Fact]
+        public void FromRoleId_ShouldFail_IfTheRoleIdDoesNotExist()
+        {
+            Should.Throw<ArgumentException>(() => TestRole.From(RoleId.From("IncorrectRole")));
+        }
+    }
+}
