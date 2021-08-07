81520614f7beda33619b66f2d42d2a0faed756f3
Francesco Dente <dente.francesco1996@gmail.com>
2021-08-07 22:28:38 +0200

    Create base class for pipeline service installer

---
+++ EasyDesk.CleanArchitecture.Web/DependencyInjection/PipelineInstallerBase.cs
@@ -0,0 +1,44 @@
+﻿using EasyDesk.CleanArchitecture.Application.Events.DependencyInjection;
+using EasyDesk.CleanArchitecture.Infrastructure.Events.ServiceBus;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace EasyDesk.CleanArchitecture.Web.DependencyInjection
+{
+    public abstract class PipelineInstallerBase : IServiceInstaller
+    {
+        protected abstract Type ApplicationAssemblyMarker { get; }
+
+        protected abstract Type InfrastructureAssemblyMarker { get; }
+
+        protected abstract Type WebAssemblyMarker { get; }
+
+        protected abstract bool UsesPublisher { get; }
+
+        protected abstract bool UsesConsumer { get; }
+
+        public void InstallServices(IServiceCollection services, IConfiguration config, IWebHostEnvironment environment)
+        {
+            services
+                .AddControllersWithPipeline(environment)
+                .AddRequestValidators(ApplicationAssemblyMarker)
+                .AddMediatorWithPipeline(ApplicationAssemblyMarker, InfrastructureAssemblyMarker)
+                .AddMappings(ApplicationAssemblyMarker, WebAssemblyMarker, InfrastructureAssemblyMarker);
+
+            if (UsesConsumer || UsesPublisher)
+            {
+                var builder = services.AddEventManagement().AddAzureServiceBus(config);
+                if (UsesPublisher)
+                {
+                    builder.AddOutboxPublisher();
+                }
+                if (UsesConsumer)
+                {
+                    builder.AddIdempotentConsumer(ApplicationAssemblyMarker);
+                }
+            }
+        }
+    }
+}
