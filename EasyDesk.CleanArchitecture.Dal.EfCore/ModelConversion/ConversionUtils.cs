ed8c53673efa9f10e0e5dd1cebe979c24a5e2a89
Francesco Dente <dente.francesco1996@gmail.com>
2021-08-26 13:50:11 +0200

    Add ApplyChangesToCollection overload accepting converter as an action

--- EasyDesk.CleanArchitecture.Dal.EfCore/ModelConversion/ConversionUtils.cs
+++ EasyDesk.CleanArchitecture.Dal.EfCore/ModelConversion/ConversionUtils.cs
@@ -16,12 +16,29 @@ namespace EasyDesk.CleanArchitecture.Dal.EfCore.ModelConversion
             where P : class, new()
             where K : IEquatable<K>
         {
+            ApplyChangesToCollection(src, srcKey, dst, dstKey, converter.ApplyChanges);
+        }
+
+        public static void ApplyChangesToCollection<D, P, K>(
+            IEnumerable<D> src,
+            Func<D, K> srcKey,
+            ICollection<P> dst,
+            Func<P, K> dstKey,
+            Action<D, P> applyChanges)
+            where P : class, new()
+            where K : IEquatable<K>
+        {
             var dstByKey = dst.ToDictionary(dstKey);
             foreach (var s in src)
             {
                 dstByKey.GetOption(srcKey(s)).Match(
-                    some: m1 => converter.ApplyChanges(s, m1),
-                    none: () => dst.Add(converter.ToPersistence(s)));
+                    some: m1 => applyChanges(s, m1),
+                    none: () =>
+                    {
+                        var p = new P();
+                        applyChanges(s, p);
+                        dst.Add(p);
+                    });
             }
 
             var srcKeys = src.Select(srcKey).ToHashSet();
