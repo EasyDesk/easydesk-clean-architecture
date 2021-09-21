d97ac2eec32dbc8165def75f5369fad5c17d4a83
ldeluigi <lucadelu97@gmail.com>
2021-09-22 00:34:18 +0200

    Add DurationSince and DurationUntil methods.

--- EasyDesk.CleanArchitecture.Domain/Time/ITimestampProvider.cs
+++ EasyDesk.CleanArchitecture.Domain/Time/ITimestampProvider.cs
@@ -1,9 +1,33 @@
 ﻿using EasyDesk.Tools.PrimitiveTypes.DateAndTime;
+using System;
 
 namespace EasyDesk.CleanArchitecture.Domain.Time
 {
     public interface ITimestampProvider
     {
         Timestamp Now { get; }
+    }
+
+    public static class TimestampProviderExtensions
+    {
+        public static Duration DurationSince(this ITimestampProvider timestampProvider, Timestamp timestamp)
+        {
+            var now = timestampProvider.Now;
+            if (timestamp > now)
+            {
+                throw new ArgumentException("The provided timestamp is in the future. Use DurationUntil(timestamp) instead.", nameof(timestamp));
+            }
+            return Duration.FromTimeOffset(now - timestamp);
+        }
+
+        public static Duration DurationUntil(this ITimestampProvider timestampProvider, Timestamp timestamp)
+        {
+            var now = timestampProvider.Now;
+            if (timestamp < now)
+            {
+                throw new ArgumentException("The provided timestamp is in the past. Use DurationSince(timestamp) instead.", nameof(timestamp));
+            }
+            return Duration.FromTimeOffset(timestamp - now);
+        }
     }
 }
