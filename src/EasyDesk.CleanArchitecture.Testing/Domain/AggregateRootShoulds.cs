3694d1407e3a75117c2da86fb77b3f80040b3356
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-08 00:13:23 +0200

    Add AggregateRootShoulds extensions

---
+++ src/EasyDesk.CleanArchitecture.Testing/Domain/AggregateRootShoulds.cs
@@ -0,0 +1,14 @@
+﻿using EasyDesk.CleanArchitecture.Domain.Metamodel;
+using Shouldly;
+
+namespace EasyDesk.CleanArchitecture.Testing.Domain
+{
+    public static class AggregateRootShoulds
+    {
+        public static void ShouldHaveEmitted<T>(this T aggregateRoot, IDomainEvent expected)
+            where T : AggregateRoot<T>
+        {
+            aggregateRoot.EmittedEvents.ShouldContain(expected);
+        }
+    }
+}
