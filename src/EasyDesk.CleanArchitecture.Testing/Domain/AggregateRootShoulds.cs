0cd11a8919ff4860e2eb0274b64f59441aa9c9b8
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-10 18:02:33 +0200

    Use ShouldlyMethods attribute

--- src/EasyDesk.CleanArchitecture.Testing/Domain/AggregateRootShoulds.cs
+++ src/EasyDesk.CleanArchitecture.Testing/Domain/AggregateRootShoulds.cs
@@ -3,6 +3,7 @@ using Shouldly;
 
 namespace EasyDesk.CleanArchitecture.Testing.Domain
 {
+    [ShouldlyMethods]
     public static class AggregateRootShoulds
     {
         public static void ShouldHaveEmitted<T>(this T aggregateRoot, IDomainEvent expected)
