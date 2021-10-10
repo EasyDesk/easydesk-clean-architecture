16e7d5b37c628e726601eb795092f46bed5f95f3
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-10 18:23:12 +0200

    Add optional unit of work to event handlers to support automatic save

--- src/EasyDesk.CleanArchitecture.Application/Mediator/DomainEventHandlerBase.cs
+++ src/EasyDesk.CleanArchitecture.Application/Mediator/DomainEventHandlerBase.cs
@@ -1,9 +1,17 @@
-﻿using EasyDesk.CleanArchitecture.Domain.Metamodel;
+﻿using EasyDesk.CleanArchitecture.Application.Data;
+using EasyDesk.CleanArchitecture.Domain.Metamodel;
 
 namespace EasyDesk.CleanArchitecture.Application.Mediator
 {
     public abstract class DomainEventHandlerBase<T> : NotificationHandlerBase<T>
         where T : IDomainEvent
     {
+        protected DomainEventHandlerBase()
+        {
+        }
+
+        protected DomainEventHandlerBase(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
     }
 }
--- src/EasyDesk.CleanArchitecture.Application/Mediator/ExternalEventHandlerBase.cs
+++ src/EasyDesk.CleanArchitecture.Application/Mediator/ExternalEventHandlerBase.cs
@@ -1,9 +1,17 @@
-﻿using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+﻿using EasyDesk.CleanArchitecture.Application.Data;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
 
 namespace EasyDesk.CleanArchitecture.Application.Mediator
 {
     public abstract class ExternalEventHandlerBase<T> : NotificationHandlerBase<T>
         where T : IExternalEvent
     {
+        protected ExternalEventHandlerBase()
+        {
+        }
+
+        protected ExternalEventHandlerBase(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+        }
     }
 }
