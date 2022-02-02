2cda13a4e00a2da0e3d01e36fccabc083293d36a
Francesco Dente <dente.francesco1996@gmail.com>
2022-02-02 23:08:14 +0100

    Map pagination DTO explicitly instead of using AutoMapper

--- src/EasyDesk.CleanArchitecture.Web/Dto/PaginationDto.cs
+++ src/EasyDesk.CleanArchitecture.Web/Dto/PaginationDto.cs
@@ -1,14 +1,10 @@
-﻿using EasyDesk.CleanArchitecture.Application.Mapping;
-using EasyDesk.CleanArchitecture.Application.Pages;
+﻿using EasyDesk.CleanArchitecture.Application.Pages;
 
 namespace EasyDesk.CleanArchitecture.Web.Dto;
 
 public record PaginationDto(int PageIndex = 0, int PageSize = Pagination.MaxPageSize)
 {
-    public class MappingToPagination : DirectMapping<PaginationDto, Pagination>
-    {
-        public MappingToPagination() : base(src => new(src.PageIndex, src.PageSize))
-        {
-        }
-    }
+    public Pagination ToPagination() => new(PageIndex, PageSize);
+
+    public static implicit operator Pagination(PaginationDto paginationDto) => paginationDto.ToPagination();
 }
