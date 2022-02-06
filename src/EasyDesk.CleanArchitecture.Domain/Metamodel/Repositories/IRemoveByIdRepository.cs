0fc36915e54d7ed6dc8644cee79b5033be14704b
Francesco Dente <dente.francesco1996@gmail.com>
2022-02-06 22:13:47 +0100

    Add remove by id repository. Closes #27

---
+++ src/EasyDesk.CleanArchitecture.Domain/Metamodel/Repositories/IRemoveByIdRepository.cs
@@ -0,0 +1,10 @@
+﻿using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using System.Threading.Tasks;
+
+namespace EasyDesk.CleanArchitecture.Domain.Metamodel.Repositories;
+
+public interface IRemoveByIdRepository<T, TId> : IGetByIdRepository<T, TId>, IRemoveRepository<T>
+    where T : AggregateRoot
+{
+    public async Task<Result<T>> RemoveById(TId id) => await GetById(id).ThenIfSuccess(Remove);
+}
