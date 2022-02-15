2c77810ec0b1a4ddf0819515c5bca495feabfed3
francescodente <dente.francesco1996@gmail.com>
2022-02-15 09:51:09 +0100

    Create Option<T> json converter

--- src/EasyDesk.CleanArchitecture.Web/Dto/ResponseDto.cs
+++ src/EasyDesk.CleanArchitecture.Web/Dto/ResponseDto.cs
@@ -1,16 +1,17 @@
 ﻿using EasyDesk.CleanArchitecture.Application.Responses;
 using EasyDesk.Tools;
+using EasyDesk.Tools.Options;
 using System.Collections.Generic;
 using System.Linq;
 using static EasyDesk.Tools.Options.OptionImports;
 
 namespace EasyDesk.CleanArchitecture.Web.Dto;
 
-public record ResponseDto<T>(T Data, object Meta, IEnumerable<ErrorDto> Errors)
+public record ResponseDto<T>(Option<T> Data, IEnumerable<ErrorDto> Errors, object Meta)
 {
-    public static ResponseDto<T> FromData(T data, object meta = null) => new(data, meta ?? Nothing.Value, Enumerable.Empty<ErrorDto>());
+    public static ResponseDto<T> FromData(T data, object meta = null) => new(Some(data), Enumerable.Empty<ErrorDto>(), meta ?? Nothing.Value);
 
-    public static ResponseDto<T> FromErrors(IEnumerable<ErrorDto> errors, object meta = null) => new(default, meta ?? Nothing.Value, errors);
+    public static ResponseDto<T> FromErrors(IEnumerable<ErrorDto> errors, object meta = null) => new(None, errors, meta ?? Nothing.Value);
 
     public static ResponseDto<T> FromError(ErrorDto error, object meta = null) => FromErrors(Some(error), meta);
 
---
+++ test/EasyDesk.CleanArchitecture.UnitTests/Infrastructure/Json/Converters/OptionConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using EasyDesk.CleanArchitecture.Infrastructure.Json.Converters;
+using EasyDesk.Tools.Options;
+using Newtonsoft.Json;
+using Shouldly;
+using Xunit;
+using static EasyDesk.Tools.Options.OptionImports;
+
+namespace EasyDesk.CleanArchitecture.UnitTests.Infrastructure.Json.Converters;
+
+public class OptionConverterTests
+{
+    private record TestRecord(int Value);
+
+    private readonly OptionConverter _sut = new();
+    private readonly TestRecord _record = new(10);
+
+    private string SerializeOption(Option<TestRecord> value) => JsonConvert.SerializeObject(value, _sut);
+
+    private Option<TestRecord> DeserializeOption(string json) => JsonConvert.DeserializeObject<Option<TestRecord>>(json, _sut);
+
+    [Fact]
+    public void ShouldSerializeNoneAsANullToken()
+    {
+        SerializeOption(None).ShouldBe(JsonConvert.Null);
+    }
+
+    [Fact]
+    public void ShouldSerializeSomeLikeTheInnerType()
+    {
+        SerializeOption(Some(_record)).ShouldBe(JsonConvert.SerializeObject(_record));
+    }
+
+    [Fact]
+    public void ShouldDeserializeNullTokenAsNone()
+    {
+        DeserializeOption(JsonConvert.Null).ShouldBeEmpty();
+    }
+
+    [Fact]
+    public void ShouldDeserializeObjectAsSome()
+    {
+        DeserializeOption(JsonConvert.SerializeObject(_record)).ShouldContain(_record);
+    }
+}
