29a5c950505b5a9a437f732491ea32ab858ef4eb
francescodente <dente.francesco1996@gmail.com>
2022-02-16 17:09:00 +0100

    Fix arguments order

--- src/EasyDesk.CleanArchitecture.Domain/Metamodel/Values/ValueWrapper.cs
+++ src/EasyDesk.CleanArchitecture.Domain/Metamodel/Values/ValueWrapper.cs
@@ -20,6 +20,6 @@ public abstract record ValueWrapper<T, S> : AbstractValueWrapper<T, S>
     }
 
     public static implicit operator T(ValueWrapper<T, S> wrapper) => wrapper is null
-        ? throw new ArgumentNullException("Implicit conversion of a null ValueWrapper.", nameof(wrapper))
+        ? throw new ArgumentNullException(nameof(wrapper), "Implicit conversion of a null ValueWrapper.")
         : wrapper.Value;
 }
