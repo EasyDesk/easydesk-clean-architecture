a42d33145674bf3fc2c2ca345b569ddc80b6599a
Luca Deluigi <lucadelu97@gmail.com>
2022-10-17 17:37:11 +0200

    Start integration with Neuroglia/AsyncApi

---
+++ src/EasyDesk.CleanArchitecture.Web/Neuroglia/AsyncApiDocumentGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Neuroglia.AsyncApi.Configuration;
+using Neuroglia.AsyncApi.Models;
+using Neuroglia.AsyncApi.Services.Generators;
+
+namespace EasyDesk.CleanArchitecture.Web.Neuroglia;
+public class AsyncApiDocumentGenerator : IAsyncApiDocumentGenerator
+{
+    public Task<IEnumerable<AsyncApiDocument>> GenerateAsync(params Type[] markupTypes)
+    {
+        return this.GenerateAsync(markupTypes, null);
+    }
+
+    public Task<IEnumerable<AsyncApiDocument>> GenerateAsync(IEnumerable<Type> markupTypes, AsyncApiDocumentGenerationOptions options)
+    {
+        if (markupTypes == null)
+        {
+            throw new ArgumentNullException(nameof(markupTypes));
+        }
+        IEnumerable<Type> types = markupTypes
+            .Select(t => t.Assembly)
+            .Distinct()
+            .SelectMany(t => t.GetTypes())
+            .Where(t => true);
+        return null;
+    }
+}
---
+++ src/EasyDesk.CleanArchitecture.Web/Startup/Modules/AsyncApiModule.cs
@@ -0,0 +1,29 @@
+﻿using EasyDesk.CleanArchitecture.Application.Modules;
+using Microsoft.Extensions.DependencyInjection;
+using Neuroglia.AsyncApi;
+using Neuroglia.AsyncApi.Configuration;
+
+namespace EasyDesk.CleanArchitecture.Web.Startup.Modules;
+public class AsyncApiModule : AppModule
+{
+    private readonly Action<IAsyncApiGenerationOptionsBuilder> _configure;
+
+    public AsyncApiModule(Action<IAsyncApiGenerationOptionsBuilder> configure = null)
+    {
+        _configure = configure;
+    }
+
+    public override void ConfigureServices(IServiceCollection services, AppDescription app)
+    {
+        services.AddAsyncApiGeneration(options =>
+        {
+            SetupAsyncApiDocs(app, options);
+            _configure?.Invoke(options);
+        });
+        services.AddAsyncApiUI("neuroglia");
+    }
+
+    private void SetupAsyncApiDocs(AppDescription app, IAsyncApiGenerationOptionsBuilder options)
+    {
+    }
+}
