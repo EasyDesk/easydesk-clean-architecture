636aaa701f71bdf8f62e7453d6d94535e7f04192
francescodente <dente.francesco1996@gmail.com>
2022-10-25 18:48:48 +0200

    Create base WebApplicationFactory for integration tests

---
+++ src/EasyDesk.CleanArchitecture.Testing.Integration/Web/IntegrationTestsWebApplicationFactory.cs
@@ -0,0 +1,42 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Xunit;
+
+namespace EasyDesk.CleanArchitecture.Testing.Integration.Web;
+
+public abstract class IntegrationTestsWebApplicationFactory<T> : WebApplicationFactory<T>, IAsyncLifetime
+    where T : class
+{
+    private readonly ISet<ITestcontainersContainer> _containers = new HashSet<ITestcontainersContainer>();
+
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureHostConfiguration(ConfigureConfiguration);
+        return base.CreateHost(builder);
+    }
+
+    protected virtual void ConfigureConfiguration(IConfigurationBuilder config)
+    {
+    }
+
+    protected TContainer RegisterTestContainer<TContainer>(Func<ITestcontainersBuilder<TContainer>, ITestcontainersBuilder<TContainer>> configureContainer)
+        where TContainer : ITestcontainersContainer
+    {
+        var container = configureContainer(new TestcontainersBuilder<TContainer>()).Build();
+        _containers.Add(container);
+        return container;
+    }
+
+    public async Task InitializeAsync()
+    {
+        await Task.WhenAll(_containers.Select(c => c.StartAsync()));
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await Task.WhenAll(_containers.Select(c => c.StopAsync()));
+    }
+}
--- test/EasyDesk.CleanArchitecture.IntegrationTests/SampleApplicationFactory.cs
+++ test/EasyDesk.CleanArchitecture.IntegrationTests/SampleApplicationFactory.cs
@@ -1,61 +1,45 @@
 ﻿using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
 using DotNet.Testcontainers.Containers;
+using EasyDesk.CleanArchitecture.Testing.Integration.Web;
 using EasyDesk.SampleApp.Web.Controllers.V_1_0;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 
 namespace EasyDesk.CleanArchitecture.IntegrationTests;
 
-public class SampleApplicationFactory : WebApplicationFactory<PersonController>, IAsyncLifetime
+public class SampleApplicationFactory : IntegrationTestsWebApplicationFactory<PersonController>
 {
     private readonly PostgreSqlTestcontainer _dbContainer;
     private readonly RabbitMqTestcontainer _rabbitMqContainer;
 
     public SampleApplicationFactory()
     {
-        _dbContainer = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+        _dbContainer = RegisterTestContainer<PostgreSqlTestcontainer>(container => container
             .WithDatabase(new PostgreSqlTestcontainerConfiguration
             {
                 Database = "TestSampleDb",
                 Username = "sample",
                 Password = "sample"
-            })
-            .Build();
+            }));
 
-        _rabbitMqContainer = new TestcontainersBuilder<RabbitMqTestcontainer>()
+        _rabbitMqContainer = RegisterTestContainer<RabbitMqTestcontainer>(container => container
             .WithMessageBroker(new RabbitMqTestcontainerConfiguration
             {
                 Username = "guest",
                 Password = "guest"
-            })
-            .Build();
+            }));
     }
 
     public string RabbitMqConnectionString => _rabbitMqContainer.ConnectionString;
 
     public string PostgresConnectionString => _dbContainer.ConnectionString;
 
-    protected override IHost CreateHost(IHostBuilder builder)
+    protected override void ConfigureConfiguration(IConfigurationBuilder config)
     {
-        builder.ConfigureHostConfiguration(config => config.AddInMemoryCollection(new Dictionary<string, string>
+        config.AddInMemoryCollection(new Dictionary<string, string>
         {
             ["ConnectionStrings:RabbitMq"] = RabbitMqConnectionString,
             ["ConnectionStrings:MainDb"] = PostgresConnectionString,
-        }));
-        return base.CreateHost(builder);
-    }
-
-    public async Task InitializeAsync()
-    {
-        await _dbContainer.StartAsync();
-        await _rabbitMqContainer.StartAsync();
-    }
-
-    async Task IAsyncLifetime.DisposeAsync()
-    {
-        await _dbContainer.DisposeAsync();
-        await _rabbitMqContainer.DisposeAsync();
+        });
     }
 }
