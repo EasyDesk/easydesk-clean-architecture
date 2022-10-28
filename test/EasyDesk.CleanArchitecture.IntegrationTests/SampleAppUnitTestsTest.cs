3d40abbd281318290e1bab6dfd58db2c77e27748
ldeluigi <lucadelu97@gmail.com>
2022-10-28 12:38:24 +0200

    Add unit test example. Closes #7

---
+++ test/EasyDesk.CleanArchitecture.IntegrationTests/SampleAppUnitTestsTest.cs
@@ -0,0 +1,23 @@
+﻿using EasyDesk.SampleApp.Application.Commands;
+using EasyDesk.SampleApp.Domain.Aggregates.PersonAggregate;
+using NodaTime;
+using NSubstitute;
+
+namespace EasyDesk.CleanArchitecture.IntegrationTests;
+
+[UsesVerify]
+public class SampleAppUnitTestsTest
+{
+    private const string TestSurname = "TestSurname";
+    private const string TestName = "TestName";
+    private static readonly LocalDate _testDate = new LocalDate(1970, 1, 1);
+
+    [Fact]
+    public async Task CreatePersonCommandTest()
+    {
+        var personRepository = Substitute.For<IPersonRepository>();
+        var handler = new CreatePerson.Handler(personRepository);
+        var result = await handler.Handle(new CreatePerson.Command(TestName, TestSurname, _testDate));
+        await Verify(result);
+    }
+}
