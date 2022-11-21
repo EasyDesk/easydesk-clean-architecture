b44458e6527fa8cdf586f0a6a995a2048cff6141
francescodente <dente.francesco1996@gmail.com>
2022-11-21 16:35:39 +0100

    Add delayed utilities to rebus helper

--- src/EasyDesk.CleanArchitecture.Testing.Integration/Rebus/RebusTestHelper.cs
+++ src/EasyDesk.CleanArchitecture.Testing.Integration/Rebus/RebusTestHelper.cs
@@ -45,55 +45,83 @@ public class RebusTestHelper : IAsyncDisposable
         _subscriptions.Remove(typeof(T));
     }
 
-    public async Task Publish<T>(T message) where T : IMessage, IEvent
-    {
+    public async Task Publish<T>(T message) where T : IMessage, IEvent =>
         await _bus.Publish(message);
-    }
 
-    public async Task Send<T>(T message) where T : IMessage, ICommand
-    {
+    public async Task Send<T>(T message) where T : IMessage, ICommand =>
         await _bus.Send(message);
-    }
 
-    public async Task Defer<T>(T message, Duration delay) where T : IMessage, ICommand
-    {
+    public async Task Defer<T>(T message, Duration delay) where T : IMessage, ICommand =>
         await _bus.Defer(delay.ToTimeSpan(), message);
-    }
 
-    private async Task Handler(IMessage message)
-    {
+    private async Task Handler(IMessage message) =>
         await _messages.Writer.WriteAsync(message);
+
+    public async Task<T> WaitForMessageAfterDelayOrFail<T>(T message, Duration delay, Duration? timeout = null) =>
+        await WaitForMessageAfterDelayOrFail<T>(m => m.Equals(message), delay, timeout);
+
+    public async Task<T> WaitForMessageAfterDelayOrFail<T>(Duration delay, Duration? timeout = null) =>
+        await WaitForMessageAfterDelayOrFail<T>(_ => true, delay, timeout);
+
+    public async Task<T> WaitForMessageAfterDelayOrFail<T>(Func<T, bool> predicate, Duration delay, Duration? timeout = null)
+    {
+        await FailIfMessageIsReceivedWithin(predicate, delay);
+        return await WaitForMessageOrFail(predicate, timeout);
     }
 
-    public async Task<T> WaitForMessageOrFail<T>(T message, Duration? timeout = null)
+    public async Task FailIfMessageIsReceivedWithin<T>(T message, Duration? timeout = null) =>
+        await FailIfMessageIsReceivedWithin<T>(m => m.Equals(message), timeout);
+
+    public async Task FailIfMessageIsReceivedWithin<T>(Duration? timeout = null) =>
+        await FailIfMessageIsReceivedWithin<T>(_ => true, timeout);
+
+    public async Task FailIfMessageIsReceivedWithin<T>(Func<T, bool> predicate, Duration? timeout = null)
     {
-        return await WaitForMessageOrFail<T>(x => x.Equals(message), timeout);
+        try
+        {
+            await WaitForMessageOrFail(predicate, timeout);
+        }
+        catch (MessageNotReceivedWithinTimeoutException)
+        {
+            return;
+        }
+
+        throw new UnexpectedMessageReceivedException(timeout ?? _defaultTimeout, typeof(T));
     }
 
-    public async Task<T> WaitForMessageOrFail<T>(Duration? timeout = null)
-    {
-        return await WaitForMessageOrFail<T>(_ => true, timeout);
-    }
+    public async Task<T> WaitForMessageOrFail<T>(T message, Duration? timeout = null) =>
+        await WaitForMessageOrFail<T>(x => x.Equals(message), timeout);
+
+    public async Task<T> WaitForMessageOrFail<T>(Duration? timeout = null) =>
+        await WaitForMessageOrFail<T>(_ => true, timeout);
 
     public async Task<T> WaitForMessageOrFail<T>(Func<T, bool> predicate, Duration? timeout = null)
+    {
+        var (m, deadLetterIndex) = await WaitForMessage(predicate, timeout);
+        deadLetterIndex.IfPresent(_deadLetter.RemoveAt);
+        return m;
+    }
+
+    private async Task<(T, Option<int>)> WaitForMessage<T>(Func<T, bool> predicate, Duration? timeout = null)
     {
         var actualTimeout = timeout ?? _timeout;
         try
         {
             var messages = _deadLetter
+                .Select((m, i) => (m, Some(i)))
                 .ToAsyncEnumerable()
                 .ThenConcat(() =>
                 {
                     var cts = new CancellationTokenSource();
                     cts.CancelAfter(actualTimeout.ToTimeSpan());
-                    return _messages.Reader.ReadAllAsync(cts.Token);
+                    return _messages.Reader.ReadAllAsync(cts.Token).Select(m => (m, NoneT<int>()));
                 });
 
-            await foreach (var m in messages)
+            await foreach (var (m, deadLetterIndex) in messages)
             {
                 if (m is T t && predicate(t))
                 {
-                    return t;
+                    return (t, deadLetterIndex);
                 }
                 _deadLetter.Add(m);
             }
---
+++ src/EasyDesk.CleanArchitecture.Testing.Integration/Rebus/UnexpectedMessageReceivedException.cs
@@ -0,0 +1,14 @@
+﻿using NodaTime;
+
+namespace EasyDesk.CleanArchitecture.Testing.Integration.Rebus;
+
+public class UnexpectedMessageReceivedException : Exception
+{
+    public UnexpectedMessageReceivedException(Duration timeout, Type messageType)
+        : base($"Message of type {messageType.Name} was received unexpectedly within the given timeout ({timeout})")
+    {
+        MessageType = messageType;
+    }
+
+    public Type MessageType { get; }
+}
