bf49e7e2729bbc02565c66824f4064c55db8c43b
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2022-12-13 15:49:11 +0100

    Update dependencies. Fix return type of SafeMoveTo and TryMoveTo

--- src/EasyDesk.CleanArchitecture.Application/Multitenancy/ITenantNavigator.cs
+++ src/EasyDesk.CleanArchitecture.Application/Multitenancy/ITenantNavigator.cs
@@ -13,19 +13,16 @@ public interface ITenantScope : IDisposable
 
 public static class TenantNavigatorExtensions
 {
-    public static async Task SafeMoveTo(this ITenantNavigator navigator, TenantId id, IMultitenancyManager multitenancyManager)
-    {
-        await navigator.TryMoveTo(id, multitenancyManager).ThenThrowIfFailure();
-    }
+    public static Task<ITenantScope> SafeMoveTo(this ITenantNavigator navigator, TenantId id, IMultitenancyManager multitenancyManager) =>
+        navigator.TryMoveTo(id, multitenancyManager).ThenThrowIfFailure();
 
-    public static async Task<Result<Nothing>> TryMoveTo(this ITenantNavigator navigator, TenantId id, IMultitenancyManager multitenancyManager)
+    public static async Task<Result<ITenantScope>> TryMoveTo(this ITenantNavigator navigator, TenantId id, IMultitenancyManager multitenancyManager)
     {
         if (!await multitenancyManager.TenantExists(id))
         {
             return new TenantNotFoundError(id);
         }
 
-        navigator.MoveToTenant(id);
-        return Ok;
+        return Success(navigator.MoveToTenant(id));
     }
 }
