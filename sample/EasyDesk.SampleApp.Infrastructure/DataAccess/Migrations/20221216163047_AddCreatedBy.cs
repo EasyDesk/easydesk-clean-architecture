0373aa58bc6da21b385ec606c2b6e24aecf0d481
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2022-12-18 22:42:56 +0100

    Fix namespace style

--- sample/EasyDesk.SampleApp.Infrastructure/DataAccess/Migrations/20221216163047_AddCreatedBy.cs
+++ sample/EasyDesk.SampleApp.Infrastructure/DataAccess/Migrations/20221216163047_AddCreatedBy.cs
@@ -2,29 +2,28 @@
 
 #nullable disable
 
-namespace EasyDesk.SampleApp.Infrastructure.DataAccess.Migrations
+namespace EasyDesk.SampleApp.Infrastructure.DataAccess.Migrations;
+
+/// <inheritdoc />
+public partial class AddCreatedBy : Migration
 {
     /// <inheritdoc />
-    public partial class AddCreatedBy : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "CreatedBy",
-                schema: "domain",
-                table: "People",
-                type: "text",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "CreatedBy",
+            schema: "domain",
+            table: "People",
+            type: "text",
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "CreatedBy",
-                schema: "domain",
-                table: "People");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "CreatedBy",
+            schema: "domain",
+            table: "People");
     }
 }
