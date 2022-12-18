d9ad9abcd55fc93d417a74c088a9d3ddb8217401
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2022-12-18 22:42:47 +0100

    Fix Option serialization with Verify

---
+++ test/EasyDesk.CleanArchitecture.IntegrationTests/VerifyConfiguration/JsonConverterFactory.cs
@@ -0,0 +1,18 @@
+﻿using Argon;
+
+namespace EasyDesk.CleanArchitecture.IntegrationTests.VerifyConfiguration;
+
+internal abstract class JsonConverterFactory : JsonConverter
+{
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        return CreateConverter(objectType, serializer).ReadJson(reader, objectType, existingValue, serializer);
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        CreateConverter(value.GetType(), serializer).WriteJson(writer, value, serializer);
+    }
+
+    protected abstract JsonConverter CreateConverter(Type objectType, JsonSerializer serializer);
+}
---
+++ test/EasyDesk.CleanArchitecture.IntegrationTests/VerifyConfiguration/VerifySettingsInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace EasyDesk.CleanArchitecture.IntegrationTests.VerifyConfiguration;
+
+public static class VerifySettingsInitializer
+{
+    [ModuleInitializer]
+    public static void Init()
+    {
+        VerifierSettings.AddExtraSettings(settings =>
+        {
+            settings.Converters.Add(new OptionConverter());
+        });
+    }
+}
