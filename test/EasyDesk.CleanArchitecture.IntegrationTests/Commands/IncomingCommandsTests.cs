a353193d17f3c72fa1cbac1ed091ebbb8b7eaf00
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-01-08 17:38:17 +0100

    Add bulk delete after tenant removal test for sample app

--- test/EasyDesk.CleanArchitecture.IntegrationTests/Commands/IncomingCommandsTests.cs
+++ test/EasyDesk.CleanArchitecture.IntegrationTests/Commands/IncomingCommandsTests.cs
@@ -1,6 +1,8 @@
 ﻿using EasyDesk.CleanArchitecture.Application.Multitenancy;
+using EasyDesk.CleanArchitecture.IntegrationTests.Api;
 using EasyDesk.CleanArchitecture.Testing.Integration.Services;
 using EasyDesk.SampleApp.Application.IncomingCommands;
+using NodaTime;
 
 namespace EasyDesk.CleanArchitecture.IntegrationTests.Commands;
 
@@ -34,5 +36,56 @@ public class IncomingCommandsTests : SampleIntegrationTest
 
         var tenantChecker = new InjectedServiceCheckBuilder<IMultitenancyManager>(WebService.Services);
         await tenantChecker.WaitUntil(async m => !await m.TenantExists(tenantId));
+    }
+
+    [Fact]
+    public async Task RemoveTenant_ShouldDeleteEntities()
+    {
+        var tenantName = "test-tenant-qwe";
+        var bus = NewBus();
+        await bus.Send(new CreateTenant(tenantName));
+
+        var tenantId = TenantId.Create(tenantName);
+        await WebService.WaitUntilTenantExists(tenantId);
+
+        var person = await Http
+            .CreatePerson(new(
+                FirstName: "Foo",
+                LastName: "Bar",
+                DateOfBirth: new LocalDate(1996, 2, 2)))
+            .Tenant(tenantName)
+            .AuthenticateAs("test-admin")
+            .Send()
+            .AsData();
+
+        await Http
+            .GetOwnedPets(person.Id)
+            .Tenant(tenantName)
+            .AuthenticateAs("test-admin")
+            .PollUntil(pets => pets.Any())
+            .EnsureSuccess();
+
+        await bus.Send(new RemoveTenant(tenantName));
+
+        var tenantChecker = new InjectedServiceCheckBuilder<IMultitenancyManager>(WebService.Services);
+        await tenantChecker.WaitUntil(async m => !await m.TenantExists(tenantId));
+
+        await bus.Send(new CreateTenant(tenantName));
+        await WebService.WaitUntilTenantExists(tenantId);
+
+        await Http
+            .GetOwnedPets(person.Id)
+            .Tenant(tenantName)
+            .AuthenticateAs("test-admin")
+            .PollUntil(pets => !pets.Any())
+            .EnsureSuccess();
+
+        var response = await Http.GetPerson(person.Id)
+            .Tenant(tenantName)
+            .AuthenticateAs("test-admin")
+            .PollWhile(w => w.IsSuccess())
+            .AsVerifiable();
+
+        await Verify(response);
     }
 }
