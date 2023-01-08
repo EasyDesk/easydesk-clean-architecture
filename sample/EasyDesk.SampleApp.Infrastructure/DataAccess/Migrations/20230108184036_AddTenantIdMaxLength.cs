dbd80926cad4b623d9bef27fa640e0ba61ecd273
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-01-08 19:45:14 +0100

    Fix migration style

--- sample/EasyDesk.SampleApp.Infrastructure/DataAccess/Migrations/20230108184036_AddTenantIdMaxLength.cs
+++ sample/EasyDesk.SampleApp.Infrastructure/DataAccess/Migrations/20230108184036_AddTenantIdMaxLength.cs
@@ -2,63 +2,62 @@
 
 #nullable disable
 
-namespace EasyDesk.SampleApp.Infrastructure.DataAccess.Migrations
+namespace EasyDesk.SampleApp.Infrastructure.DataAccess.Migrations;
+
+/// <inheritdoc />
+public partial class AddTenantIdMaxLength : Migration
 {
     /// <inheritdoc />
-    public partial class AddTenantIdMaxLength : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                schema: "domain",
-                table: "Pets",
-                type: "character varying(256)",
-                maxLength: 256,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            schema: "domain",
+            table: "Pets",
+            type: "character varying(256)",
+            maxLength: 256,
+            nullable: false,
+            defaultValue: string.Empty,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                schema: "domain",
-                table: "People",
-                type: "character varying(256)",
-                maxLength: 256,
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            schema: "domain",
+            table: "People",
+            type: "character varying(256)",
+            maxLength: 256,
+            nullable: false,
+            defaultValue: string.Empty,
+            oldClrType: typeof(string),
+            oldType: "text",
+            oldNullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                schema: "domain",
-                table: "Pets",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(256)",
-                oldMaxLength: 256,
-                oldDefaultValue: "");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            schema: "domain",
+            table: "Pets",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(256)",
+            oldMaxLength: 256,
+            oldDefaultValue: string.Empty);
 
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                schema: "domain",
-                table: "People",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "character varying(256)",
-                oldMaxLength: 256,
-                oldDefaultValue: "");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            schema: "domain",
+            table: "People",
+            type: "text",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "character varying(256)",
+            oldMaxLength: 256,
+            oldDefaultValue: string.Empty);
     }
 }
