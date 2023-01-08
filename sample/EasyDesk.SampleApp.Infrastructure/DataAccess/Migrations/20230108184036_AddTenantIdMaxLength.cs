774cdb9d4dcdb7e08fd24deee23470cdcc78217e
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-01-08 19:44:17 +0100

    Update sample app migrations

---
+++ sample/EasyDesk.SampleApp.Infrastructure/DataAccess/Migrations/20230108184036_AddTenantIdMaxLength.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyDesk.SampleApp.Infrastructure.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTenantIdMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                schema: "domain",
+                table: "Pets",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                schema: "domain",
+                table: "People",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                schema: "domain",
+                table: "Pets",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldDefaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TenantId",
+                schema: "domain",
+                table: "People",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldDefaultValue: "");
+        }
+    }
+}
