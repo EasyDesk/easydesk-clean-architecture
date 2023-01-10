09d66e13543e7c0813f1508e17c2926c58c2275b
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-01-10 20:40:57 +0100

    Move polling exception in its own file

---
+++ src/EasyDesk.CleanArchitecture.Testing.Integration/Commons/Polling/PollingFailedException.cs
@@ -0,0 +1,11 @@
+﻿using NodaTime;
+
+namespace EasyDesk.CleanArchitecture.Testing.Integration.Commons.Polling;
+
+public class PollingFailedException : Exception
+{
+    public PollingFailedException(int attempts, Duration timeout)
+        : base($"Polling timed out. Attempted {attempts} polls in {timeout.TotalSeconds:0.000} seconds")
+    {
+    }
+}
