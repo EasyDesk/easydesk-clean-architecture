e8f4517f4726015e6bee2530b7f11d51536b87d1
francescodente <dente.francesco1996@gmail.com>
2023-05-05 13:15:09 +0200

    Use auto property

--- src/ca/EasyDesk.CleanArchitecture.Application/Multitenancy/TenantService.cs
+++ src/ca/EasyDesk.CleanArchitecture.Application/Multitenancy/TenantService.cs
@@ -2,18 +2,22 @@
 
 public class TenantService : ITenantNavigator, IContextTenantInitializer
 {
-    private Option<TenantInfo> _contextTenantInfo = None;
     private Option<TenantInfo> _overriddenTenantInfo = None;
 
     public void Initialize(TenantInfo tenantInfo)
     {
-        if (_contextTenantInfo.IsPresent)
+        if (ContextTenantInfo.IsPresent)
         {
             throw new InvalidOperationException("Trying to initialize tenant after it was already initialized");
         }
 
-        _contextTenantInfo = Some(tenantInfo);
+        ContextTenantInfo = Some(tenantInfo);
     }
+
+    public Option<TenantInfo> ContextTenantInfo { get; private set; } = None;
+
+    public TenantInfo TenantInfo => _overriddenTenantInfo.OrElse(
+        ContextTenantInfo.OrElseThrow(() => new InvalidOperationException("Accessing tenant before initialization")));
 
     public void MoveToTenant(TenantId id) => MoveToTenantInfo(Some(TenantInfo.Tenant(id)));
 
@@ -23,15 +27,10 @@ public class TenantService : ITenantNavigator, IContextTenantInitializer
 
     private void MoveToTenantInfo(Option<TenantInfo> tenantInfo)
     {
-        if (_contextTenantInfo.IsAbsent)
+        if (ContextTenantInfo.IsAbsent)
         {
             throw new InvalidOperationException("Trying to move to a different tenant before initialization");
         }
         _overriddenTenantInfo = tenantInfo;
     }
-
-    public TenantInfo TenantInfo => _overriddenTenantInfo.OrElse(
-        _contextTenantInfo.OrElseThrow(() => new InvalidOperationException("Accessing tenant before initialization")));
-
-    public Option<TenantInfo> ContextTenantInfo => _contextTenantInfo;
 }
