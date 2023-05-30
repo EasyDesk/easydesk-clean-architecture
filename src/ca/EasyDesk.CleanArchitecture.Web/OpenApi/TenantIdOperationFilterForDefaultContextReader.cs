68f374005d601e768253913cf153a7c8944cf183
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-05-30 17:18:53 +0200

    Improve support for non-nullable reference types and for multitenancy in open api

---
+++ src/ca/EasyDesk.CleanArchitecture.Web/OpenApi/TenantIdOperationFilterForDefaultContextReader.cs
@@ -0,0 +1,24 @@
+﻿using EasyDesk.CleanArchitecture.Infrastructure.Multitenancy;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace EasyDesk.CleanArchitecture.Web.OpenApi;
+
+internal class TenantIdOperationFilterForDefaultContextReader : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        operation.Parameters ??= new List<OpenApiParameter>();
+        if (!operation.Parameters.Any(p => p.Name.Equals(MultitenancyDefaults.TenantIdHttpQueryParam)))
+        {
+            operation.Parameters.Add(new OpenApiParameter()
+            {
+                Name = MultitenancyDefaults.TenantIdHttpQueryParam,
+                In = ParameterLocation.Query,
+                Description = $"Optional parameter alternative to setting the {MultitenancyDefaults.TenantIdHttpHeader} header for specifying the tenant id of the request, with lower priority than the header.",
+                Required = false,
+                AllowEmptyValue = true,
+            });
+        }
+    }
+}
