b603ba195fcdc1bdaa18a23bac12c6c757ef551e
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-06-21 19:00:36 +0200

    Add RuleForEachOption

--- src/ca/EasyDesk.CleanArchitecture.Application/Validation/PimpedAbstractValidator.cs
+++ src/ca/EasyDesk.CleanArchitecture.Application/Validation/PimpedAbstractValidator.cs
@@ -13,10 +13,18 @@ public abstract class PimpedAbstractValidator<T> : AbstractValidator<T>
         {
             rules(RuleFor(x => propertySelector(x).Value)
                 .SetValidator(new InlineValidator<TProperty>())
-                .OverridePropertyName(property.GetMember().Name));
+                .OverridePropertyName(property.GetMember()?.Name ?? $"{typeof(TProperty).Name}"));
         });
     }
 
     public void RuleForOption<TProperty>(Expression<Func<T, Option<TProperty>>> property, IValidator<TProperty> validator) =>
         RuleForOption(property, rules => rules.SetValidator(validator));
+
+    public void RuleForEachOption<TProperty>(Expression<Func<T, IEnumerable<Option<TProperty>>>> properties, Func<IRuleBuilderOptions<T, TProperty>, IRuleBuilderOptions<T, TProperty>> rules)
+    {
+        var propertiesSelector = properties.Compile();
+        rules(RuleForEach(x => propertiesSelector(x).SelectMany(o => o))
+            .SetValidator(new InlineValidator<TProperty>())
+            .OverridePropertyName(properties.GetMember()?.Name ?? $"Every {typeof(TProperty).Name}"));
+    }
 }
