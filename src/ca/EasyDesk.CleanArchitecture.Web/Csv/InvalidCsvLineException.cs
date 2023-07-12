ef20556873db0df95b75edc4b80cbe5de2f17792
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-07-12 10:09:05 +0200

    Simplify throwing custom parse errors in csv service

---
+++ src/ca/EasyDesk.CleanArchitecture.Web/Csv/InvalidCsvLineException.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyDesk.CleanArchitecture.Web.Csv;
+
+public class InvalidCsvLineException : Exception
+{
+    public InvalidCsvLineException(string field, string message)
+        : base(message)
+    {
+        Field = field;
+    }
+
+    public string Field { get; }
+}
