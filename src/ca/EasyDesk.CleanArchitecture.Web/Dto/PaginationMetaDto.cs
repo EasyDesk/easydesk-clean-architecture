d804381658503706adf4642ac871967ca6791fa7
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-07-31 16:35:46 +0200

    Remove method

--- src/ca/EasyDesk.CleanArchitecture.Web/Dto/PaginationMetaDto.cs
+++ src/ca/EasyDesk.CleanArchitecture.Web/Dto/PaginationMetaDto.cs
@@ -5,11 +5,8 @@ public record PaginationMetaDto(
     int PageIndex,
     int PageSize)
 {
-    public static PaginationMetaDto FromFailure(int pageSize, int pageIndex) =>
-        new(Count: 0, PageIndex: pageIndex, PageSize: pageSize);
-
     public static PaginationMetaDto FromResult<T>(Result<IEnumerable<T>> result, int pageSize, int pageIndex) =>
-        result.Match(
+        result.Match<PaginationMetaDto>(
             success: t => new(Count: t.Count(), PageIndex: pageIndex, PageSize: pageSize),
-            failure: _ => FromFailure(pageSize, pageIndex));
+            failure: _ => new(Count: 0, PageIndex: pageIndex, PageSize: pageSize));
 }
