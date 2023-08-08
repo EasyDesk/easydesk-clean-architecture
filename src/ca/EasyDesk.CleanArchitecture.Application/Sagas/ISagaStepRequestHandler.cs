7e77a1e27e77f3f254e13bfa16e1f7ea9be6af54
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-08-08 09:50:48 +0200

    Fix interface naming

--- src/ca/EasyDesk.CleanArchitecture.Application/Sagas/ISagaStepRequestHandler.cs
+++ src/ca/EasyDesk.CleanArchitecture.Application/Sagas/ISagaStepRequestHandler.cs
@@ -1,5 +1,4 @@
 ﻿using EasyDesk.CleanArchitecture.Application.Dispatching;
-using EasyDesk.CleanArchitecture.Domain.Metamodel;
 
 namespace EasyDesk.CleanArchitecture.Application.Sagas;
 
@@ -9,7 +8,7 @@ public interface ISagaStepRequestHandler<T, R, TId, TState>
     Task<Result<R>> Handle(T request, SagaContext<TId, TState> context);
 }
 
-public interface ISagaStepHandler<T, TId, TState> : ISagaStepRequestHandler<T, Nothing, TId, TState>
+public interface ISagaStepRequestHandler<T, TId, TState> : ISagaStepRequestHandler<T, Nothing, TId, TState>
     where T : IDispatchable<Nothing>
 {
 }
