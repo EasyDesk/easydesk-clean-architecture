e525c09d7b0bed892f9a7a1d067a7da94b063243
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-08-17 15:43:58 +0200

    Add MoveTo extension emthods

--- src/ca/EasyDesk.CleanArchitecture.Application/Multitenancy/ITenantNavigator.cs
+++ src/ca/EasyDesk.CleanArchitecture.Application/Multitenancy/ITenantNavigator.cs
@@ -26,4 +26,10 @@ public static class TenantNavigatorExtensions
         navigator.MoveToTenant(id);
         return Ok;
     }
+
+    public static void MoveTo(this ITenantNavigator navigator, TenantInfo tenantInfo) =>
+        tenantInfo.Id.Match(some: navigator.MoveToTenant, none: navigator.MoveToPublic);
+
+    public static void MoveTo(this ITenantNavigator navigator, Option<TenantInfo> tenantInfo) =>
+        tenantInfo.Match(some: navigator.MoveTo, none: navigator.MoveToContextTenant);
 }
