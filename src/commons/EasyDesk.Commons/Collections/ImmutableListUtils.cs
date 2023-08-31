e4594ce3b9c3e1e1b592b2eded8918b1e94a72bb
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2023-08-31 15:28:28 +0200

    Add binary search utility methods for immutable lists

---
+++ src/commons/EasyDesk.Commons/Collections/ImmutableListUtils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Immutable;
+
+namespace EasyDesk.Commons.Collections;
+
+public static class ImmutableListUtils
+{
+    public static int BinarySearchItemOrPrevious<T>(this ImmutableList<T> list, T key)
+    {
+        var result = list.BinarySearch(key);
+        if (result >= 0)
+        {
+            return result;
+        }
+        result = ~result - 1;
+        return result >= 0 ? result : 0;
+    }
+
+    public static int BinarySearchItemOrNext<T>(this ImmutableList<T> list, T key)
+    {
+        var result = list.BinarySearch(key);
+        if (result >= 0)
+        {
+            return result;
+        }
+        result = ~result;
+        return result < list.Count ? result : list.Count - 1;
+    }
+}
