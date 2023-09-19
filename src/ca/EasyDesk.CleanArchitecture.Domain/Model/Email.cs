d8c9ef80d90be4fec31e965a11f0e9d72ae18241
francescodente <dente.francesco1996@gmail.com>
2023-09-19 18:47:03 +0200

    Revert to using hard coded email address regex

--- src/ca/EasyDesk.CleanArchitecture.Domain/Model/Email.cs
+++ src/ca/EasyDesk.CleanArchitecture.Domain/Model/Email.cs
@@ -1,9 +1,10 @@
 ﻿using EasyDesk.CleanArchitecture.Domain.Metamodel.Values;
 using FluentValidation;
+using System.Text.RegularExpressions;
 
 namespace EasyDesk.CleanArchitecture.Domain.Model;
 
-public record Email : PureValue<string, Email>, IValue<string>
+public partial record Email : PureValue<string, Email>, IValue<string>
 {
     public const int MaxLength = 254;
 
@@ -13,5 +14,8 @@ public record Email : PureValue<string, Email>, IValue<string>
 
     public static IRuleBuilder<X, string> ValidationRules<X>(IRuleBuilder<X, string> rules) => rules
         .MaximumLength(MaxLength)
-        .EmailAddress();
+        .DependentRules(() => rules.Matches(Regex()));
+
+    [GeneratedRegex("^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])?$")]
+    public static partial Regex Regex();
 }
