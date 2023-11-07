2556b160d224b787049b16ce7c962c4030555a89
francescodente <dente.francesco1996@gmail.com>
2023-11-07 16:18:22 +0100

    Add collection validators for duplicate detection

---
+++ src/ca/EasyDesk.CleanArchitecture.Domain/Metamodel/Values/Validation/CollectionValidationExtensions.cs
@@ -0,0 +1,36 @@
+﻿using EasyDesk.Commons.Collections;
+using EasyDesk.Commons.Options;
+using FluentValidation;
+using FluentValidation.Internal;
+using System.Linq.Expressions;
+
+namespace EasyDesk.CleanArchitecture.Domain.Metamodel.Values.Validation;
+
+public static class CollectionValidationExtensions
+{
+    public static IRuleBuilder<T, IEnumerable<TItem>> NotContainingDuplicatesFor<T, TItem, TProperty>(this IRuleBuilder<T, IEnumerable<TItem>> rule, Expression<Func<TItem, TProperty>> property, IEqualityComparer<TProperty>? comparer = null)
+    {
+        var member = property.GetMember();
+        var func = AccessorCache<TItem>.GetCachedAccessor(member, property);
+        var propertyName = ValidatorOptions.Global.PropertyNameResolver(typeof(TItem), member, property);
+        return rule
+            .Must((_, xs, ctx) => FindDuplicate(xs.Select(func), comparer)
+                .IfPresent(p => ctx.MessageFormatter.AppendArgument("DuplicateValue", p))
+                .IsAbsent)
+            .WithErrorCode("NotContainingDuplicatesFor")
+            .WithMessage($$"""'{PropertyName}' contains duplicate values for property '{{propertyName}}'.""");
+    }
+
+    public static IRuleBuilder<T, IEnumerable<TItem>> NotContainingDuplicates<T, TItem>(this IRuleBuilder<T, IEnumerable<TItem>> rule, IEqualityComparer<TItem>? comparer = null) => rule
+        .Must((_, xs, ctx) => FindDuplicate(xs, comparer)
+            .IfPresent(p => ctx.MessageFormatter.AppendArgument("DuplicateValue", p))
+            .IsAbsent)
+        .WithErrorCode("NotContainingDuplicates")
+        .WithMessage($$"""'{PropertyName}' contains duplicate values.""");
+
+    private static Option<T> FindDuplicate<T>(IEnumerable<T> items, IEqualityComparer<T>? comparer)
+    {
+        var set = new HashSet<T>(comparer);
+        return items.FirstOption(x => !set.Add(x));
+    }
+}
---
+++ test/ca/EasyDesk.CleanArchitecture.UnitTests/Domain/Metamodel/CollectionValidationExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using EasyDesk.CleanArchitecture.Application.Validation;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Values.Validation;
+using FluentValidation;
+
+namespace EasyDesk.CleanArchitecture.UnitTests.Domain.Metamodel;
+
+[UsesVerify]
+public class CollectionValidationExtensionsTests
+{
+    public record Model(params string[] Values);
+
+    [Fact]
+    public async Task NotContainingDuplicates_ShouldReturnFailure_IfCollectionContainsDuplicates()
+    {
+        await Validate(new("A", "AA", "AA"), x => x.NotContainingDuplicates());
+    }
+
+    [Fact]
+    public async Task NotContainingDuplicates_ShouldReturnSuccess_IfCollectionContainsNoDuplicates()
+    {
+        await Validate(new("A", "B", "C"), x => x.NotContainingDuplicates());
+    }
+
+    [Fact]
+    public async Task NotContainingDuplicatesFor_ShouldReturnFailure_IfCollectionContainsDuplicates()
+    {
+        await Validate(new("A", "AA", "BB"), x => x.NotContainingDuplicatesFor(x => x.Length));
+    }
+
+    [Fact]
+    public async Task NotContainingDuplicatesFor_ShouldReturnSuccess_IfCollectionContainsNoDuplicates()
+    {
+        await Validate(new("A", "BB", "CCC"), x => x.NotContainingDuplicatesFor(x => x.Length));
+    }
+
+    private static async Task Validate(Model model, Action<IRuleBuilder<Model, IEnumerable<string>>> configure)
+    {
+        var validator = new PimpedInlineValidator<Model>()
+            .Also(x => configure(x.RuleFor(x => x.Values)));
+
+        await Verify(validator.Validate(model));
+    }
+}
