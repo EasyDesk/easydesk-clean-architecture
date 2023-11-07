9a858ff7dbdd381e60e2d674471e4f4043049085
francescodente <dente.francesco1996@gmail.com>
2023-11-07 16:18:04 +0100

    Add kotlin-like scope functions

--- src/commons/EasyDesk.Commons/StaticImports.cs
+++ src/commons/EasyDesk.Commons/StaticImports.cs
@@ -6,6 +6,22 @@ public static partial class StaticImports
 {
     public static T It<T>(T t) => t;
 
+    public static R Let<T, R>(this T self, Func<T, R> function) => function(self);
+
+    public static async Task<R> Let<T, R>(this T self, AsyncFunc<T, R> function) => await function(self);
+
+    public static T Also<T>(this T self, Action<T> action)
+    {
+        action(self);
+        return self;
+    }
+
+    public static async Task<T> Also<T>(this T self, AsyncAction<T> action)
+    {
+        await action(self);
+        return self;
+    }
+
     public static Nothing ReturningNothing(Action action)
     {
         action();
