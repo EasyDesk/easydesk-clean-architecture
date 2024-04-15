5b9286c4a49550da613b6ae2265f0234e05b87d6
francescodente <dente.francesco1996@gmail.com>
2024-04-15 19:05:09 +0200

    Add in memory mapping to projectables

--- src/ca/EasyDesk.CleanArchitecture.Dal.EfCore/Interfaces/IProjectable.cs
+++ src/ca/EasyDesk.CleanArchitecture.Dal.EfCore/Interfaces/IProjectable.cs
@@ -5,6 +5,10 @@ namespace EasyDesk.CleanArchitecture.Dal.EfCore.Interfaces;
 public interface IProjectable<F, T>
     where F : IProjectable<F, T>
 {
+    private static readonly Lazy<Func<F, T>> _inMemoryMapper = new(() => F.Projection().Compile());
+
+    public static T MapInMemory(F src) => _inMemoryMapper.Value(src);
+
     static abstract Expression<Func<F, T>> Projection();
 }
 
