04fafcd5c66bfdd7f65552941090350dac1de8b7
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-07-09 11:24:09 +0200

    Fix ui culture in culture info extensions

--- src/commons/EasyDesk.Commons/Globalization/CultureInfoExtensions.cs
+++ src/commons/EasyDesk.Commons/Globalization/CultureInfoExtensions.cs
@@ -4,59 +4,51 @@ namespace EasyDesk.Commons.Globalization;
 
 public static class CultureInfoExtensions
 {
-    public static void Use(this CultureInfo culture, Action action)
-    {
-        var currentCulture = CultureInfo.CurrentCulture;
-        try
+    public static void Use(this CultureInfo culture, Action action) =>
+        Use(culture, () =>
         {
-            CultureInfo.CurrentCulture = culture;
             action();
-        }
-        finally
-        {
-            CultureInfo.CurrentCulture = currentCulture;
-        }
-    }
+            return Nothing.Value;
+        });
 
     public static T Use<T>(this CultureInfo culture, Func<T> func)
     {
         var currentCulture = CultureInfo.CurrentCulture;
+        var currentUICulture = CultureInfo.CurrentUICulture;
         try
         {
             CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
             return func();
         }
         finally
         {
             CultureInfo.CurrentCulture = currentCulture;
+            CultureInfo.CurrentUICulture = currentUICulture;
         }
     }
 
-    public static async Task UseAsync(this CultureInfo culture, Func<Task> action)
-    {
-        var currentCulture = CultureInfo.CurrentCulture;
-        try
+    public static Task UseAsync(this CultureInfo culture, Func<Task> action) =>
+        UseAsync(culture, async () =>
         {
-            CultureInfo.CurrentCulture = culture;
             await action();
-        }
-        finally
-        {
-            CultureInfo.CurrentCulture = currentCulture;
-        }
-    }
+            return Nothing.Value;
+        });
 
     public static async Task<T> UseAsync<T>(this CultureInfo culture, Func<Task<T>> func)
     {
         var currentCulture = CultureInfo.CurrentCulture;
+        var currentUICulture = CultureInfo.CurrentUICulture;
         try
         {
             CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
             return await func();
         }
         finally
         {
             CultureInfo.CurrentCulture = currentCulture;
+            CultureInfo.CurrentUICulture = currentUICulture;
         }
     }
 }
