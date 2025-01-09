3a22eed6711b48ae35451ebc840db550d597cd22
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-01-09 12:36:55 +0100

    Add abstract AppBuilder to DependencyInjectionModule

--- src/ca/EasyDesk.CleanArchitecture.Web/CleanArchitectureAppBuilder.cs
+++ src/ca/EasyDesk.CleanArchitecture.Web/CleanArchitectureAppBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using EasyDesk.CleanArchitecture.DependencyInjection.Modules;
 using EasyDesk.Commons.Collections;
-using EasyDesk.Commons.Collections.Immutable;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,23 +7,18 @@ using Microsoft.Extensions.Diagnostics.Metrics;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System.CommandLine;
-using System.Reflection;
-using static EasyDesk.Commons.Collections.ImmutableCollections;
 
 namespace EasyDesk.CleanArchitecture.Web;
 
-public sealed class CleanArchitectureAppBuilder : IAppBuilder
+public sealed class CleanArchitectureAppBuilder : AppBuilder, IAppBuilder
 {
-    private readonly ModulesCollection _modules = new();
-    private IFixedSet<Assembly> _assemblies = Set<Assembly>();
-    private string _name;
     private readonly string[] _args;
     private readonly WebApplicationBuilder _applicationBuilder;
     private Action<WebApplication>? _configureWebApplication;
 
     internal CleanArchitectureAppBuilder(string name, string[] args, WebApplicationBuilder applicationBuilder)
+        : base(name)
     {
-        _name = name;
         _args = args;
         _applicationBuilder = applicationBuilder;
     }
@@ -37,48 +31,15 @@ public sealed class CleanArchitectureAppBuilder : IAppBuilder
 
     public IMetricsBuilder Metrics => _applicationBuilder.Metrics;
 
-    public IAppBuilder WithName(string name)
-    {
-        _name = name;
-        return this;
-    }
-
-    public IAppBuilder WithAssemblies(IEnumerable<Assembly> assemblies)
-    {
-        _assemblies = _assemblies.Union(assemblies);
-        return this;
-    }
-
-    public IAppBuilder AddModule<T>(T module) where T : AppModule
-    {
-        _modules.AddModule(module);
-        return this;
-    }
-
-    public IAppBuilder RemoveModule<T>() where T : AppModule
-    {
-        _modules.RemoveModule<T>();
-        return this;
-    }
-
-    public IAppBuilder ConfigureModule<T>(Action<T> configure) where T : AppModule
-    {
-        var module = _modules
-            .GetModule<T>()
-            .OrElseThrow(() => new RequiredModuleMissingException(typeof(T)));
-        configure(module);
-        return this;
-    }
-
     public IAppBuilder ConfigureWebApplication(Action<WebApplication> configure)
     {
         _configureWebApplication += configure;
         return this;
     }
 
-    public async Task<int> Run()
+    public override async Task<int> Run()
     {
-        var appDescription = new AppDescription(_name, _modules, _assemblies);
+        var appDescription = BuildAppDescription();
 
         _applicationBuilder.Services.AddSingleton(appDescription);
 
