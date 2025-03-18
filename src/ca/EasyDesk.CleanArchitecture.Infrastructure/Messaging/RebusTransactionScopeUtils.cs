dc7729ed347e3ffa3d5c4c634768cb304b63709d
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-03-18 16:15:50 +0100

    Make RebusTransactionScopeUtils public

--- src/ca/EasyDesk.CleanArchitecture.Infrastructure/Messaging/RebusTransactionScopeUtils.cs
+++ src/ca/EasyDesk.CleanArchitecture.Infrastructure/Messaging/RebusTransactionScopeUtils.cs
@@ -3,7 +3,7 @@ using Rebus.Transport;
 
 namespace EasyDesk.CleanArchitecture.Infrastructure.Messaging;
 
-internal static class RebusTransactionScopeUtils
+public static class RebusTransactionScopeUtils
 {
     public static RebusTransactionScope CreateScopeWithComponentContext(IComponentContext context)
     {
