02387c062dd86ba79c4c535b52254d41f83056b5
francescodente <dente.francesco1996@gmail.com>
2025-03-24 18:01:05 +0100

    Add one of validator

---
+++ src/ca/EasyDesk.CleanArchitecture.Application/Validation/ValidationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using EasyDesk.Commons.Collections;
+using FluentValidation;
+
+namespace EasyDesk.CleanArchitecture.Application.Validation;
+
+public static class ValidationExtensions
+{
+    public static IRuleBuilderOptions<T, TProperty> OneOf<T, TProperty>(this IRuleBuilder<T, TProperty> builder, params IEnumerable<TProperty> values) => builder
+        .Must(x => values.Contains(x))
+        .WithErrorCode("OneOf")
+        .WithMessage($"{{PropertyName}} must be one of {values.ToListString()}");
+}
