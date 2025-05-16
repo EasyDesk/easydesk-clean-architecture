c53bcd0189f2fb608e86dd78c14293dd856af99d
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2025-05-16 10:52:22 +0200

    Add instant to messaging inbox

--- src/ca/EasyDesk.CleanArchitecture.Dal.EfCore/Messaging/InboxMessage.cs
+++ src/ca/EasyDesk.CleanArchitecture.Dal.EfCore/Messaging/InboxMessage.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NodaTime;
 
 namespace EasyDesk.CleanArchitecture.Dal.EfCore.Messaging;
 
 internal class InboxMessage
 {
     public required string Id { get; set; }
+
+    public required Instant Instant { get; set; }
 
     public sealed class Configuration : IEntityTypeConfiguration<InboxMessage>
     {
