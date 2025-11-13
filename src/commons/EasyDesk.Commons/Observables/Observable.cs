707d59cd21a479966d409fc21722ffcc7d3744a9
francescodente <dente.francesco1996@gmail.com>
2025-11-13 11:04:07 +0100

    Add extensions for mapping, filtering and composing observables

---
+++ src/commons/EasyDesk.Commons/Observables/Observable.cs
@@ -0,0 +1,44 @@
+﻿using EasyDesk.Commons.Collections;
+using EasyDesk.Commons.Collections.Immutable;
+
+namespace EasyDesk.Commons.Observables;
+
+public static class Observable
+{
+    public static ISubscription Subscribe<T>(this IObservable<T> observable, Action handler) =>
+        observable.Subscribe(_ => handler());
+
+    public static IObservable<R> Map<T, R>(this IObservable<T> observable, Func<T, R> mapper) =>
+        Custom<R>(h => observable.Subscribe(t => h(mapper(t))));
+
+    public static IObservable<T> Filter<T>(this IObservable<T> observable, Func<T, bool> predicate) =>
+        Custom<T>(h => observable.Subscribe(t =>
+        {
+            if (predicate(t))
+            {
+                h(t);
+            }
+        }));
+
+    public static IObservable<T> Compose<T>(params IFixedList<IObservable<T>> observables) =>
+        Custom<T>(h =>
+        {
+            var subscriptions = observables.Select(o => o.Subscribe(h));
+            return new Subscription(() => subscriptions.ForEach(s => s.Unsubscribe()));
+        });
+
+    private static IObservable<T> Custom<T>(Func<Action<T>, ISubscription> subscribe) =>
+        new CustomObservable<T>(subscribe);
+
+    private class CustomObservable<T> : IObservable<T>
+    {
+        private readonly Func<Action<T>, ISubscription> _subscribe;
+
+        public CustomObservable(Func<Action<T>, ISubscription> subscribe)
+        {
+            _subscribe = subscribe;
+        }
+
+        public ISubscription Subscribe(Action<T> handler) => _subscribe(handler);
+    }
+}
