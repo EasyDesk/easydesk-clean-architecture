54fc696e6904ff39b413b525542e30b6cfe1108c
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2026-05-28 12:00:55 +0200

    Remove sqlite and fix openapi for option types

--- src/ca/EasyDesk.CleanArchitecture.Web/OpenApi/OptionSchemaFilter.cs
+++ src/ca/EasyDesk.CleanArchitecture.Web/OpenApi/OptionSchemaFilter.cs
@@ -1,4 +1,5 @@
 ﻿using EasyDesk.Commons.Options;
+using EasyDesk.Commons.Reflection;
 using Microsoft.OpenApi;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
@@ -9,13 +10,14 @@ internal class OptionSchemaFilter : ISchemaFilter
     public void Apply(IOpenApiSchema schema, SchemaFilterContext context)
     {
         var type = context.Type;
-        if (!type.IsGenericType || !type.GetGenericTypeDefinition().IsAssignableTo(typeof(Option<>)) || schema is not OpenApiSchema concreteSchema)
+        if (!type.IsGenericType || !type.GetGenericTypeDefinition().IsSubtypeOrImplementationOf(typeof(Option<>)) || schema is not OpenApiSchema concreteSchema)
         {
             return;
         }
         var wrappedType = type.GetGenericArguments()[0];
         var wrappedSchema = context.SchemaGenerator.GenerateSchema(wrappedType, context.SchemaRepository);
         concreteSchema.CopyFunctionalFieldsFrom(wrappedSchema);
-        concreteSchema.Type |= JsonSchemaType.Null;
+        concreteSchema.Type = JsonSchemaType.Null | JsonSchemaType.Object;
+        concreteSchema.AllOf = [wrappedSchema,];
     }
 }
